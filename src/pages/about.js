10794fab1bcd5466522bbecd4c1fac664a829476
Tales Scopinho <talico@Taless-MacBook-Pro.local>
2025-01-20 21:29:13 -0800

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,18 +17,18 @@ export default function About() {
                 <div className={styles.greeting}>
                   <h2 style={{display: 'inline' }}>Heyo!</h2> <h3 style={{display: 'inline' }}>   welcome to my plaground.</h3>
                 </div>
-                <p>I'm a computer scientist by schooling, yet my interests are quite wide. Some of my latest projects that I particularly enjoyed working one are:</p>
+                <p>I'm a computer scientist by schooling, yet my interests are quite wide. Some of my latest projects I particularly enjoyed working on are:</p>
                 <ul>
                   <li className={styles.list}><Link to="https://github.com/talicopanda/social-media-influence-analysis"><i>Analysing Influence on Social Media</i></Link></li>
                   <ul>
-                    <li className={styles.sublist}>A research project under Professor Peter Marbach to study how people influence and are influenced by people in their communities on Twitter.</li>
+                    <li className={styles.sublist}>A research project under Professor Peter Marbach to study how people influence and get influenced by people in their Twitter communities.</li>
                   </ul>
                   <li className={styles.list}><Link to="https://github.com/Hennmeister/kv-store"><i>Building a Database from Scratch</i></Link></li>
                   <ul>
-                    <li className={styles.sublist}>A project under the guidance of Professor Niv Dayan that aims to build an efficient and robust key-value store database entirely from scratch in C++.</li>
+                    <li className={styles.sublist}>A project under the guidance of Professor Niv Dayan for building an efficient and robust key-value store database from scratch in C++.</li>
                   </ul>
                 </ul>
-                <p>Currently at <span style={{fontSize: '22px', fontWeight: 'bold'}}>Microsoft</span> and graduate of the <span style={{fontSize: '22px', fontWeight: 'bold'}}>University of Toronto</span>.</p>
+                <p>Currently a Software Engineer at <span style={{fontSize: '22px', fontWeight: 'bold'}}>Microsoft</span> and a graduate of the <span style={{fontSize: '22px', fontWeight: 'bold'}}>University of Toronto</span>.</p>
               </div>
           </section>
         </div>
